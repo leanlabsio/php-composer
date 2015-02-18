bacdc05fe4f53696aa4d9d421a84707728f2aaab
cnam <cnam812@gmail.com>
2015-02-18 21:47:52 +0400

    fixed composer.sh

--- composer.sh
+++ composer.sh
@@ -4,9 +4,5 @@ set -e
 composer update --no-dev -o
 
 while inotifywait -e modify composer.json >/dev/null 2>&1; do
-      RATIO=305 #I got this by dividing the intel max_brightness by acpi's max_brightness+1 
-      BRIGHTNESS=`cat composer.json`
-      if [ $BRIGHTNESS -eq "0" ]; then BRIGHTNESS=1; fi
-      NEW_BRIGHTNESS=$((RATIO*BRIGHTNESS))
       composer update --no-dev -o	
 done
